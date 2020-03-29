3126a3919c321fa89e1235704a8c6c263c75fdf3
Theinfinix500 <jbaraali18@gmail.com>
2020-03-29 20:41:55 +0100

    added a second page that shows the movie details, and the first page shows a list of movies at the inial load that are hard coded and then the user can type a movie name and the result will be showed as a card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { HttpClientModule } from "@angular/common/http";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { MovieDetailsPageModule } from "../pages/movie-details/movie-details.module";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    MovieDetailsPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
